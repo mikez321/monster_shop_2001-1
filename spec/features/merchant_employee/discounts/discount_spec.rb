3169d2b027ef8b804acbdc5f4a08a1b3aa57276e
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-17 12:46:36 -0600

    Begin test for bulk discounts and test for change of git remote

---
+++ spec/features/merchant_employee/discounts/discount_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe "bulk discounts" do
+  it "has a bulk discount page" do
+    # visit "/merchant/discounts"
+    # expect(page).to have_content("Discounts")
+  end
+end
