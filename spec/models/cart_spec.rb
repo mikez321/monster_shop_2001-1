382e75ae8d0fcd470892333ecac46082086ab816
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-18 16:47:19 -0600

    Change data type of item ID for testing

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -7,20 +7,20 @@ RSpec.describe Cart do
     @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
     @rusty_chain = @bike_shop.items.create(name: "Rusty Chain", description: "Its old and covered in rust.", price: 1, image: "https://thumbs.dreamstime.com/z/rusty-bicycle-chain-picture-blog-rusty-bicycle-chain-picture-blog-background-title-132179093.jpg", inventory: 1, active?:false)
     @cart = Cart.new({
-      @chain.id => 4,
-      @rusty_chain.id => 1
+      @chain.id.to_s => 4,
+      @rusty_chain.id.to_s => 1
     })
     @discount_cart = Cart.new({
-      @chain.id => 5,
-      @rusty_chain.id => 1
+      @chain.id.to_s => 5,
+      @rusty_chain.id.to_s => 1
     })
   end
 
   describe "instance methods" do
     it "contents" do
       expected = {
-        @chain.id => 4,
-        @rusty_chain.id => 1
+        @chain.id.to_s => 4,
+        @rusty_chain.id.to_s => 1
       }
 
       expect(@cart.contents).to eq(expected)
