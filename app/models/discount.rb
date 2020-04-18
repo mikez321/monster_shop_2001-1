bc814c3ffebe6e58d3c1a3f6636b82b36a8369a6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-18 16:46:10 -0600

    Add destroy dependents to merchant discount CRUD functions

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,5 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
-  has_many :discount_items
+  has_many :discount_items, dependent: :destroy
   has_many :items, through: :discount_items
 end
