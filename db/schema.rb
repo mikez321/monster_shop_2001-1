254e586596e96afbd8ffd4a00ea3493c4782675a
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-17 19:22:04 -0600

    Create discount table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200414032852) do
+ActiveRecord::Schema.define(version: 20200417212116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "discounts", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "amount"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "item_orders", force: :cascade do |t|
     t.bigint "order_id"
