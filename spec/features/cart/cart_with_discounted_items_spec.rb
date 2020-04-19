12ea2a53bae7dd60fbab17d8e87960d0d92a9855
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-19 12:43:26 -0600

    Fix name typo

--- spec/features/cart/cart_with_discounted_items_spec.rb
+++ spec/features/cart/cart_with_discounted_items_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "as a user, when I visit my cart, if I have items that qualify fo
     @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
     @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
     @rusty_chain = @bike_shop.items.create(name: "Rusty Chain", description: "Its old and covered in rust.", price: 1, image: "https://thumbs.dreamstime.com/z/rusty-bicycle-chain-picture-blog-rusty-bicycle-chain-picture-blog-background-title-132179093.jpg", inventory: 1, active?:false)
-    @bike_shop.discounts.create(n ame: "10 for 5", description: "Recieve 10% off an item when you purchase 5 or more", amount: 10, quantity: 5)
+    @bike_shop.discounts.create(name: "10 for 5", description: "Recieve 10% off an item when you purchase 5 or more", amount: 10, quantity: 5)
 
     @user = User.create!(name: "Josh Tukman",
                           address: "756 Main St.",
